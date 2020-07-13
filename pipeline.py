157b9330ae96daab9033a5b38b267d32dfc40324
Анна Иголкина <anna@Annas-MacBook-Pro.local>
2020-07-14 00:40:18 +0300

    setaria example

--- pipeline.py
+++ pipeline.py
@@ -1,4 +1,5 @@
 import glob
+import  csv
 
 from Bio import motifs
 from Bio import SeqIO
@@ -27,6 +28,7 @@ def get_best_score(seq, ppm):
 
 path_jaspar = 'jaspar/'
 path_motifs = 'motifs/'
+file_out = 'best_motifs.csv'
 
 # Read ppm in jaspar format
 files_jaspar = glob.glob(path_jaspar + '*.jaspar')
@@ -35,12 +37,13 @@ ppms = []
 pfms = []
 for f in files_jaspar:
     with open(f) as fh:
-        pfms += [motifs.read(fh, "jaspar")]
+        pfms += [motifs.read(fh, 'jaspar')]
         ppms += [motifs.matrix.PositionWeightMatrix('ACGT', pfms[-1].counts)]
 
 # Read motifs
 files_motifs = glob.glob(path_motifs + '*.fasta')
 records = [item for f in files_motifs for item in list(SeqIO.parse(f, "fasta"))]
+list(SeqIO.parse(files_motifs[1], "fasta"))
 
 
 # Get best ppm for each motif
@@ -48,7 +51,10 @@ best_motifs = []
 for record in records:
     ppm_scores = [get_best_score(record, ppm) for ppm in ppms]
     max_id = ppm_scores.index(max(ppm_scores))
-    best_motifs += [[pfms[max_id].matrix_id, pfms[max_id].name, max(ppm_scores)]]
+    best_motifs += [ record.name.split('_') + [pfms[max_id].matrix_id, pfms[max_id].name, max(ppm_scores)]]
 
 
 
+with open(file_out, 'w') as f:
+    wr = csv.writer(f)
+    wr.writerows(best_motifs)
